d0ff38b413672433c4a557de559e4270b1618876
Pedro Paiva <phpaiva0905@gmail.com>
2026-03-26 09:11:07 -0300

    email error

--- servidor/server.js
+++ servidor/server.js
@@ -53,10 +53,15 @@ db.connect(err => {
 
 // ================== EMAIL ==================
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // Use true para a porta 465, false para outras
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false // Ajuda a evitar bloqueios de certificados em nuvem
     }
 });
 
